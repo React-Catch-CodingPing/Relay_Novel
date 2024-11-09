b246bd32d2692df1efcd1ca44a5cf8a3920c8ab7
Han bi <hanbi5553@naver.com>
2024-11-09 16:34:25 +0900

    메인화면 UI

--- relay-noval/src/App.js
+++ relay-noval/src/App.js
@@ -3,18 +3,31 @@ import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setUser } from "./store/authSlice";
-import {auth, firestore} from "./firebase/firebase";
+import { auth, firestore } from "./firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
+// 기존에 추가된 컴포넌트들
 import Home from "./components/Home";
 import SignUp from "./components/Auth/SignUp";
 import SignIn from "./components/Auth/SignIn";
-import {doc, getDoc} from "firebase/firestore";
+
+// 새로 추가된 컴포넌트들
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import WorksSection from "./components/WorksSection";
+import AuthorsSection from "./components/AuthorsSection";
+import Footer from "./components/Footer";
+
+// 추가한 페이지 컴포넌트들
+import HallOfFame from "./components/HallOfFame";
+import Genres from "./components/Genres";
+import Authors from "./components/Authors";
+import Community from "./components/Community";
+import StartNovel from "./components/StartNovel";
 
 const App = () => {
-
     const dispatch = useDispatch();
-
 
     useEffect(() => {
         // Firebase 인증 상태 변경을 감지하여 Redux 상태에 저장
@@ -37,17 +50,40 @@ const App = () => {
         return () => unsubscribe();
     }, [dispatch]);
 
-
     return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<SignIn />} />
-          {/* 추가할 경로들을 여기에 정의 */}
-        </Routes>
-      </Router>
-  );
+        <Router>
+            <div className="App">
+                {/* 공통 네비게이션 바 */}
+                <Navbar />
+
+                <Routes>
+                    {/* Home 페이지 */}
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <Header />
+                                <WorksSection />
+                                <AuthorsSection />
+                                <Footer />
+                            </>
+                        }
+                    />
+
+                    {/* 회원가입 및 로그인 페이지 */}
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/login" element={<SignIn />} />
+
+                    {/* 추가된 메뉴 페이지 */}
+                    <Route path="/hall-of-fame" element={<HallOfFame />} />
+                    <Route path="/genres" element={<Genres />} />
+                    <Route path="/authors" element={<Authors />} />
+                    <Route path="/community" element={<Community />} />
+                    <Route path="/start-novel" element={<StartNovel />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
